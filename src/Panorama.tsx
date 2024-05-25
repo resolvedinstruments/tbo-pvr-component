48bab57aae0ac554c3a464c295841a53b6c527a9
Callum Doolin <callum@resolvedinstruments.com>
2024-05-25 16:18:33 -0600

    change aspect ratio

--- src/Panorama.tsx
+++ src/Panorama.tsx
@@ -7,33 +7,40 @@ import { useRef } from "preact/hooks"
 export const Panorama = () => {
   const panoramaImage = "example_stitched.jpg"
   const previewImage = panoramaImage.replace("stitched.jpg", "thumbnail.png")
-  const imgRef = useRef(null)
+  const divRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     // calculate vaov from ponomara image dimensions
     const width = 20891
     const height = 2160
-    const vaov = (height / width) * 360
+    const vaov = (height / width) * 360 * 1
 
-    // fudge factor to remove edges from image
-    const vaov2 = vaov * 0.92
+    let aspectRatio = 2.5
+    if (divRef.current) {
+      const { clientWidth, clientHeight } = divRef.current
+      aspectRatio = clientWidth / clientHeight
+    }
+
+    const hfov =
+      2 *
+      Math.atan(Math.tan((vaov / 2) * (Math.PI / 180)) * aspectRatio) *
+      (180 / Math.PI)
 
     pannellum.viewer("panorama", {
       type: "equirectangular",
       panorama: panoramaImage,
       preview: previewImage,
       vaov,
-      minPitch: -vaov2 / 2,
-      maxPitch: vaov2 / 2,
+      minPitch: -vaov / 2,
+      maxPitch: vaov / 2,
       showFullscreenCtrl: false,
       showZoomCtrl: false,
       autoLoad: true,
-      minHfov: 40,
-      maxHfov: (vaov2 * 16) / 9,
-      dynamic: false,
+      minHfov: 30,
+      maxHfov: hfov * 1.0,
     })
   })
 
-  return <div id="panorama" className={container}></div>
+  return <div id="panorama" className={container} ref={divRef}></div>
 }
 export default Panorama
--- src/styles.css.ts
+++ src/styles.css.ts
@@ -3,5 +3,5 @@ import { style } from "@vanilla-extract/css"
 export const container = style({
   width: "100%",
   position: "relative",
-  aspectRatio: "16/9",
+  aspectRatio: "5/2",
 })
